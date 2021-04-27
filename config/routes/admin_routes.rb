c63641b709e41146d45878814150c6fafdaba6c5
Sub-Xaero <Sub-Xaero@users.noreply.github.com>
2021-04-27 23:47:36 +0100

    Fix admin devise routes

--- config/routes/admin_routes.rb
+++ config/routes/admin_routes.rb
@@ -6,7 +6,15 @@ module AdminRoutes
   def self.extended(router)
     router.instance_exec do
       namespace :admin do
-        devise_for :users, path: "/", class_name: "Admin::User"
+        devise_for :users,
+                   path: "/",
+                   class_name: "Admin::User",
+                   only: %i[ sessions passwords unlocks],
+                   controllers: {
+                     passwords: "admin/devise/user/passwords",
+                     sessions: "admin/devise/user/sessions",
+                     unlocks: "admin/devise/user/unlocks"
+                   }
 
         get "dashboard", to: "dashboard#index"
 
